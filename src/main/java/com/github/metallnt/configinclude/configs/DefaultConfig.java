ca1990b794232ab54feef8baac6badcb763e9c36
”Metall” <serzvasington@gmail.com>
2021-12-24 19:28:22 +0500

    add "get" in config

--- src/main/java/com/github/metallnt/configinclude/configs/DefaultConfig.java
+++ src/main/java/com/github/metallnt/configinclude/configs/DefaultConfig.java
@@ -2,6 +2,7 @@ package com.github.metallnt.configinclude.configs;
 
 import com.github.metallnt.configinclude.CleanUp;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
@@ -46,4 +47,43 @@ public class DefaultConfig extends AbstractConfig {
         return this.getConfig().getBoolean("update_config", true);
     }
 
+    public boolean getDeleteDirs() {
+        return this.getConfig().getBoolean("delete_dirs.enable", true);
+    }
+
+    public int getPeriodDeleteDirs() {
+        return this.getConfig().getInt("delete_dirs.period_days");
+    }
+
+    public boolean getPurgeData() {
+        return this.getConfig().getBoolean("purge_data.enable", true);
+    }
+
+    public int getPeriodPurgeData() {
+        return this.getConfig().getInt("purge_data.period_month");
+    }
+
+    public boolean getClearDatabase() {
+        return this.getConfig().getBoolean("database.enable", true);
+    }
+
+    public int getNonPluginPeriod() {
+        return this.getConfig().getInt("database.period_month_non_plugin");
+    }
+
+    public int getWithPluginPeriod() {
+        return this.getConfig().getInt("database.period_month_with_plugin");
+    }
+
+    public String getTypeDatabase() {
+        return this.getConfig().getString("database.type_database");
+    }
+
+    public List getProtectionPlayers() {
+        return (List) this.getConfig().getStringList("protection_players");
+    }
+
+    public List getProtectionPlugins() {
+        return (List) this.getConfig().getStringList("protection_plugins");
+    }
 }
